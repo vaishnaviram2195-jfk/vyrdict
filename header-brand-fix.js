f6c6e0b811bd77573dac618802b01b316c8cab23
vaishnaviram2195-jfk <vaishnaviram2195@gmail.com>
2026-08-23 17:46:11 -0400

    Refresh homepage simplification loader

--- header-brand-fix.js
+++ header-brand-fix.js
@@ -32,7 +32,7 @@
   if(document.getElementById('vyrdict-home-simplify-loader'))return;
   const s=document.createElement('script');
   s.id='vyrdict-home-simplify-loader';
-  s.src='/homepage-simplify.js?v=4';
+  s.src='/homepage-simplify.js?v=5';
   s.defer=true;
   document.head.appendChild(s);
 })();
